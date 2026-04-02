2f0e493aa6f7ddd78e1da56b3eb37d0f29ff0819
khunz <arjun.iceburnt@gmail.com>
2026-04-02 15:56:06 +0100

    Completed JS Foundation

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') return 'ERROR';
+    if (a < 0 || b < 0) return 'ERROR';
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
 
+    let sum = 0;
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+        sum += i;
+    }
+    return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people
+        .map(person => {
+            const deathYear = person.yearOfDeath || new Date().getFullYear();
+            return {...person, age: deathYear - person.yearOfBirth};
+        })
+        .reduce((oldest, current) => current.age > oldest.age ? current : oldest);
 };
+
+// Do not edit below this line
+module.exports = findTheOldest;
 
 // Do not edit below this line
 module.exports = findTheOldest;
